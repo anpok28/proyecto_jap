e7315a92e4fc5e3351195b5ef66dcc2ec17f2c65
anpok28 <68907665+anpok28@users.noreply.github.com>
2020-08-15 16:11:47 -0300

    Login y validación en Js
    Pagina de login con validación en js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,28 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+//document.addEventListener("DOMContentLoaded", function(e){
 
+//});
+var correoElectronico = document.getElementById("email");
+var password = document.getElementById("password");
+var form = document.getElementById("ingresar");
+var error = document.getElementById("error");
+error.style.color = "red";
+
+form.addEventListener("submit", function (event) {
+  //console.log("enviando formulario.....");
+  event.preventDefault();
+
+  if (correoElectronico.value.length != 0 && password.value.length != 0) {
+    return (location.href = "index.html");
+  } else var mensajeError = [];
+
+  if (correoElectronico.value === null || correoElectronico.value === "") {
+    mensajeError.push("Ingrese un Correo Electrónico");
+  }
+  if (password.value === null || password.value === "") {
+    mensajeError.push("Ingrese una Contraseña");
+  }
+  error.innerHTML = mensajeError.join(", ");
 });
