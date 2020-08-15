437fbfbda2ec1d3b0674c10c624cad394f652a5b
anpok28 <68907665+anpok28@users.noreply.github.com>
2020-08-15 00:14:25 -0300

    Listado Productos
    Agregue listado de Productos, tambien un titulo de referencia.

--- js/products.js
+++ js/products.js
@@ -1,6 +1,46 @@
+// Defino la Variable en la cual quedaran los datos del JSON cuya URL : PRODUCTS_URL
+var currentProductsArray = [];
+
+function showProductsList(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentProductsArray.length; i++){
+        let products = currentProductsArray[i];
+        
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ products.name +`</h4>
+                            <small class="text-muted">` + products.productCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + products.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+        }
+
+        document.getElementById("products-list-container").innerHTML = htmlContentToAppend;
+    }
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            //console.log(resultObj.data);
+            currentProductsArray=resultObj.data;
+        }
+    showProductsList();
 
+});
 });
