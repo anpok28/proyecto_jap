a471882ffbd311619c49a8fb6dc9c0ef0b3d8086
anpok28 <68907665+anpok28@users.noreply.github.com>
2020-09-12 22:02:49 -0300

    Agregue Product Info

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,30 @@
+//Realizar una petición web a una URL con un identificador del producto donde se encuentra toda la información
+// detallada del mismo. Toda la información se deberá desplegar en HTML.
+
+//Realizar una petición web a una URL donde ya se encuentran los comentarios y puntuación precargados del
+//producto del punto anterior. Mostrar en HTML la información. ¿Quieres aplicar la puntuación en formato de
+//estrellas?, entra aquí.
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+  var product_info = {};
+  getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      //console.log(resultObj.data);
+      //IGUALO resultObj.data a una Variable
+      product_info = resultObj.data;
+      //console.log(product_info_object);
+      document.getElementById("prod-info-name").innerHTML = product_info.name;
+    }
+    
+  });
+  getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
 
+
+
+  });
+
+  
 });
